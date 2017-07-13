554964f4ee8c2405508b3ea024dc890bf54239de
15017103 <15017103@myrp.edu.sg>
2017-07-13 15:45:52 +0800

    initial commit

--- app/src/main/java/sg/edu/rp/c346/p08_map/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/p08_map/MainActivity.java
@@ -1,13 +1,186 @@
 package sg.edu.rp.c346.p08_map;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.PermissionChecker;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn1, btn2, btn3;
+    private GoogleMap map;
+    Spinner spnBranch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        spnBranch = (Spinner)findViewById(R.id.spinnerBranch);
+
+        spnBranch.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                if (spnBranch.getSelectedItemPosition() == 1) {
+                    btn1.performClick();
+                } else if (spnBranch.getSelectedItemPosition() == 2) {
+                    btn2.performClick();
+                } else if (spnBranch.getSelectedItemPosition() == 3) {
+                    btn3.performClick();
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        FragmentManager fm = getSupportFragmentManager();
+        SupportMapFragment mapFragment = (SupportMapFragment)
+                fm.findFragmentById(R.id.map);
+
+        mapFragment.getMapAsync(new OnMapReadyCallback(){
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                map = googleMap;
+
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        1);
+
+                LatLng poi_CausewayPoint = new LatLng(1.352083, 103.819836);
+                map.moveCamera(CameraUpdateFactory.newLatLngZoom(poi_CausewayPoint,
+                        11));
+                UiSettings ui = map.getUiSettings();
+                ui.setCompassEnabled(true);
+                ui.setZoomControlsEnabled(true);
+            }
+        });
+
+        btn1 = (Button) findViewById(R.id.btn1);
+        btn2 = (Button) findViewById(R.id.btn2);
+        btn3 = (Button) findViewById(R.id.btn3);
+
+
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LatLng poi_HQ = new LatLng(1.461474, 103.827667);
+                Marker rp = map.addMarker(new
+                        MarkerOptions()
+                        .position(poi_HQ)
+                        .title("HQ - North")
+                        .snippet("Block 333, Admiralty Ave 3, 765654")
+                        .icon(BitmapDescriptorFactory.fromResource(android.R.drawable.btn_star_big_on)));
+                map.moveCamera(CameraUpdateFactory.newLatLngZoom(poi_HQ,
+                        13));
+                map.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+                    @Override
+                    public boolean onMarkerClick(Marker marker) {
+                        Toast.makeText(MainActivity.this, "HQ - North", Toast.LENGTH_SHORT).show();
+                        return false;
+                    }
+                });
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LatLng poi_Central = new LatLng(1.298129, 103.847651);
+                Marker rp = map.addMarker(new
+                        MarkerOptions()
+                        .position(poi_Central)
+                        .title("Central")
+                        .snippet("Block 3A, Orchard Ave 3, 134542 ")
+                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_VIOLET)));
+                map.moveCamera(CameraUpdateFactory.newLatLngZoom(poi_Central,
+                        13));
+
+                map.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+                    @Override
+                    public boolean onMarkerClick(Marker marker) {
+                        Toast.makeText(MainActivity.this, "Central", Toast.LENGTH_SHORT).show();
+                        return false;
+                    }
+                });
+            }
+        });
+
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LatLng poi_East = new LatLng(1.350389, 103.932724);
+                Marker rp = map.addMarker(new
+                        MarkerOptions()
+                        .position(poi_East)
+                        .title("East")
+                        .snippet("Block 555, Tampines Ave 3, 287788")
+                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)));
+                map.moveCamera(CameraUpdateFactory.newLatLngZoom(poi_East,
+                        13));
+                map.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+                    @Override
+                    public boolean onMarkerClick(Marker marker) {
+                        Toast.makeText(MainActivity.this, "East", Toast.LENGTH_SHORT).show();
+                        return false;
+                    }
+                });
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    int permissionCheck = ContextCompat.checkSelfPermission(MainActivity.this,
+                            android.Manifest.permission.ACCESS_FINE_LOCATION);
+
+                    if (permissionCheck == PermissionChecker.PERMISSION_GRANTED) {
+                        map.setMyLocationEnabled(true);
+                        Log.e("GMap - Permission", "GPS access has been granted");
+                    } else {
+                        Log.e("GMap - Permission", "GPS access has not been granted");
+                    }
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Toast.makeText(MainActivity.this, "Permission denied to read your External storage", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
     }
 }
